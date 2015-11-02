9988749b5b5a85168ef45f53b26632a97d47e581
LeoIM <leo@leoim.com>
2015-11-02 14:15:12 -0800

    updated gameobject

--- pango.js
+++ pango.js
@@ -1,4 +1,15 @@
 //pango.js
 
+function GameObj(options){
+  this.pos=options.pos||[0,0,0];
+  //position             x,y,z
+  this.scl=options.scl||[1,1,1];
+  //scale                x,y,z
+  this.rot=options.rot||[0,0,0];
+  
+  this.children=options.children||[];
+
+}
+
 function Entity(options){
 }
